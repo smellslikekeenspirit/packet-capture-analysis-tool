e2b892b0fd69246c74e8ab7517503f09f559dd91
Prionti <pdn3628@rit.edu>
2022-04-24 17:14:31 -0400

    updated metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,69 +1,52 @@
-import os
-import re
-import pandas as pd
+import packet_parser
+
 
 def compute(nodes):
-	requestSent = 0
-	requestRecv = 0
-	repliesSent = 0
-	repliesRecv = 0
-	requestByte = 0
-	repliesByte = 0
-	requestData = 0
-	repliesData = 0
+    request_sent = 0
+    request_recv = 0
+    replies_sent = 0
+    replies_recv = 0
+    request_bytes_sent = 0
+    request_bytes_recv = 0
+    replies_byte = 0
+    requestData = 0
+    repliesData = 0
 
-	totalRTT = 0
-	totalHop = 0
+    totalRTT = 0
+    totalHop = 0
 
-	print('called compute function in in compute_metrics.py')
-	#Split all the information from the dictionaries into a massive list, remove blank spaces, newline characters, |, etc
-	fields = nodes[1].to_markdown().split(' ')
-	key = []
-	for item in fields:
-		if item != '' and item != '|' and item != '\n' and item != '|\n|':
-			key.append(item)
-	#remove the header of the dictionary
-	for i in range(0, 14):
-		del key[0]
-	n = 11
-	#Compute the information. If i % 11 is 0 then it's the start of a new entry. Add (1:10) to get other information in that line
-	for i in range(len(key)):
-		if i % n == 0:
-			"""
-			key[i + 1] == No.
-                        key[i + 2] == Time
-                        key[i + 3] == Source
-			key[i + 4] == Destination
-                        key[i + 5] == Length
-                        key[i + 6] == Type (Request/Reply)
-			key[i + 7] == ID
-                        key[i + 8] == Seq
-                        key[i + 9] == TTL
-			key[i + 10] == Associated Request/Reply No.
-			"""
-			if key[i + 3] == '192.168.100.1' and key[i + 3] != key[i + 4] and key[i + 6] == 'request':
-				requestSent = requestSent + 1
-				requestByte = requestByte + int(key[i + 5])
-			elif key[i + 3] == '192.168.100.1' and key[i + 3] != key[i + 4] and key[i + 6] == 'reply':
-				repliesSent = repliesSent + 1
-			elif key[i + 4] == '192.168.100.1' and key[i + 3] != key[i + 4] and key[i + 6] == 'request':
-				requestRecv = requestRecv + 1
-			elif key[i + 4] == '192.168.100.1' and key[i + 3] != key[i + 4] and key[i + 6] == 'reply':
-				repliesRecv = repliesRecv + 1
-			else:
-				pass
+    # list of node IP addresses where index i represents IP of node i+1 (index 0 = IP of node 1)
+    node_IPs = ['192.168.100.1', '192.168.100.2', '192.168.200.1', '192.168.200.2']
 
-			if key[i + 6] == 'request':
-				requestByte = requestByte + int(key[i + 5])
-			elif key[i + 6] == 'reply':
-				repliesByte = repliesByte + int(key[i + 5])
-			else:
-				pass
+    print('called compute function in in compute_metrics.py')
+    for node_number in nodes.keys():
+        print("Node ", node_number)
+        # dataframe for this node
+        df = nodes[node_number]
+        for index, data in df.iterrows():
+            if data['Source'] == node_IPs[node_number-1] and data['Source'] != data['Destination'] \
+                    and data['Type (Request/Reply)'] == 'request':
+                request_sent = request_sent + 1
+                request_bytes_sent = request_bytes_sent + int(data['Length'])
+            elif data['Source'] == node_IPs[node_number-1] and data['Source'] != data['Destination'] \
+                    and data['Type (Request/Reply)'] == 'reply':
+                replies_sent = replies_sent + 1
+            elif data['Source'] != node_IPs[node_number-1] and data['Source'] != data['Destination'] \
+                    and data['Type (Request/Reply)'] == 'request':
+                request_recv = request_recv + 1
+                request_bytes_recv = request_bytes_recv + int(data['Length'])
+            elif data['Source'] != node_IPs[node_number-1] and data['Source'] != data['Destination'] \
+                    and data['Type (Request/Reply)'] == 'reply':
+                replies_recv = replies_recv + 1
+            else:
+                pass
 
-			
-	print(requestSent / 4)
-	print(repliesSent / 4)
-	print(requestRecv / 4)
-	print(repliesRecv / 4)
-	print(requestByte)
-	print(repliesByte)
+        print(request_sent)
+        print(request_recv)
+        print(replies_sent)
+        print(replies_recv)
+        print(request_bytes_sent)
+        print(request_bytes_recv)
+
+
+compute(packet_parser.parse())
